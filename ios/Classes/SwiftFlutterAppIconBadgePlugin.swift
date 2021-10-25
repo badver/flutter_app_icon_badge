5dc54c0b239abe4622f413b172e40a97a6b4b7e1
frank <fh@codin-it.de>
2021-10-25 11:34:45 +0200

    requestAuthorization extracted

--- ios/Classes/SwiftFlutterAppIconBadgePlugin.swift
+++ ios/Classes/SwiftFlutterAppIconBadgePlugin.swift
@@ -8,39 +8,71 @@ public class SwiftFlutterAppIconBadgePlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+  public func handle(_ call: FlutterMethodCall, result: FlutterResult) {
     switch call.method {
-        case "updateBadge":
-          if let args = call.arguments as? Dictionary<String, Any>,
-            let count = args["count"] as? Int {
-            UNUserNotificationCenter.current().requestAuthorization(options: .badge){ 
-              (granted, error) in
-                if error == nil {
-                  UIApplication.shared.applicationIconBadgeNumber = count
-                }
-            }
-            result(nil)
-          } else {
-            result(FlutterError.init(code: "bad args", message: nil, details: nil))
-          }
-        case "removeBadge":
-            UNUserNotificationCenter.current().requestAuthorization(options: .badge){
-              (granted, error) in
-                if error == nil {
-                    UIApplication.shared.applicationIconBadgeNumber = 0
-                }
-            }
-          result(nil)
-        case "isAppBadgeSupported":
-        var isSupported:Bool = false
-            UNUserNotificationCenter.current().requestAuthorization(options: .badge){
-              (granted, error) in
-                isSupported = (error == nil)
-            }
+    case "updateBadge":
+      if let args = call.arguments as? [String: Any],
+         let count = args["count"] as? Int
+      {
+        requestAuthorization(onSuccess: {
+            UIApplication.shared.applicationIconBadgeNumber = count
+        })
 
-          result(isSupported)
-        default:
-          result(FlutterMethodNotImplemented)
+        result(nil)
+      } else {
+        result(FlutterError(code: "bad args", message: nil, details: nil))
+      }
+    case "removeBadge":
+      requestAuthorization(onSuccess: {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+      })
+      result(nil)
+    case "isAppBadgeSupported":
+      result(true)
+      
+    default:
+      result(FlutterMethodNotImplemented)
     }
   }
+
+  func requestAuthorization(onSuccess: @escaping () -> Void) {
+    if #available(iOS 10, *) {
+      let center = UNUserNotificationCenter.current()
+      center.requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in
+          DispatchQueue.main.async {
+              // run in main thread
+              onSuccess()
+          }
+      }
+    } else {
+      let notificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      UIApplication.shared.registerUserNotificationSettings(notificationSettings)
+        DispatchQueue.main.async {
+            // run in main thread
+            onSuccess()
+        }
+
+    }
+  }
+
+  // func handle(_ call: FlutterMethodCall?, result: FlutterResult) {
+  //     enableNotifications()
+
+  //     if "updateBadge" == call?.method {
+  //         let args = call?.arguments
+  //         let count = args?["count"] as? NSNumber
+  //         UIApplication.shared.applicationIconBadgeNumber = count?.intValue ?? 0
+  //         result(nil)
+  //     } else if "removeBadge" == call?.method {
+  //         UIApplication.shared.applicationIconBadgeNumber = 0
+  //         result(nil)
+  //     } else if "isAppBadgeSupported" == call?.method {
+  //         result(NSNumber(value: true))
+
+  //     } else if "isAppBadgeAllowed" == call?.method{
+  //       result(enableNotifications())
+  //     } else {
+  //         result(FlutterMethodNotImplemented)
+  //     }
+  // }
 }
